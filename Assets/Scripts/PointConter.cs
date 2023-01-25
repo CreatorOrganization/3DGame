3797e36897e68ec8f579bb7f5cf1969b8d411d13
Uchidakouei <kouei28poiou@icloud.com>
2023-01-25 11:31:19 +0900

    Cheanged 2023/01/25

--- Assets/Scripts/PointConter.cs
+++ Assets/Scripts/PointConter.cs
@@ -16,9 +16,9 @@ public class PointConter : MonoBehaviour
          SetScore(); //スコア計算用代入して表示
      }
 
-     private void OnCollisionEnter(Collider collision)
+     private void OnCollisionEnter(Collision collision)
      {
-         if (transform.gameObject.tag == "Player")
+         if (transform.gameObject.tag == "Player" && collision.gameObject.CompareTag("Finish"))
          {
              score += 10;
              Debug.Log("Helo Word!!");
