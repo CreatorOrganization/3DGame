e3595b452125aefb78da67f8ec10f06942d626bb
uchidakouei <kouei28poiou@icloud.com>
2023-02-12 00:12:54 +0900

    Cheanged 2023/02/12

--- Assets/Scripts/PointConter.cs
+++ Assets/Scripts/PointConter.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PointConter : MonoBehaviour
 {
@@ -23,12 +24,13 @@ public class PointConter : MonoBehaviour
              score += 10;
          }
          SetScore();
+
          if(transform.gameObject.tag == "EditorOnly")
          {
             score +=30;
          }
-         SetScore();
-     }
+         SetScore();          
+    }
      void SetScore()
      {
          ScoreText.text = string.Format("現在の点数:{0}", score);
